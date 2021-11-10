27682ea2ebdab3b95de0473e0ce9b96b08c6c0d5
quangnln <quangnln@ait.com>
2021-11-10 16:16:15 +0700

    update

---
+++ src/main/java/com/ait/manager/model/User.java
@@ -0,0 +1,72 @@
+package com.ait.manager.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean deleted = false;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+
+    @ManyToOne(targetEntity = Role.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public User(String username, String password, String fullName, Role role) {
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.role = role;
+    }
+    
+    public User(String username, String password, Role role) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", roles=" + role +
+                '}';
+    }
+    
+    
+}
---
+++ src/main/java/com/ait/manager/service/user/UserService.java
@@ -0,0 +1,33 @@
+package com.ait.manager.service.user;
+
+
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.ait.manager.model.User;
+import com.ait.manager.security.UserPrincipal;
+
+public interface UserService extends UserDetailsService {
+    Iterable<User> findAll();
+
+    User createUser(User user);
+
+    Optional<User> findUserById(Long id);
+
+    UserPrincipal findByUsername(String username);
+
+    boolean isContainUsername(String username);
+
+    Iterable<User> findAllByDeletedFalse();
+
+    Iterable<User> findAllByDeletedTrue();
+
+    User save(User user);
+
+    User findByName(String username);
+
+    void remove(Long id);
+
+    Iterable<User> findByUserRoleStaff(Long id);
+}
